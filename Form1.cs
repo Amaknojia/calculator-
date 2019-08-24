970c6f8a0550ca48caf7b405d767be7fd241ee52
Amaknojia <53323277+Amaknojia@users.noreply.github.com>
2019-08-24 14:38:04 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "1";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "3";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "0";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + ".";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "+";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "=";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "-";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = String.Empty;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "X";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "4";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "9";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text + "/";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = textBoxResult.Text.Remove(textBoxResult.Text.Length - 1);
+
+        }
+
+    }
+}
